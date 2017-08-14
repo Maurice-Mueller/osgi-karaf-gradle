29704103511676868a1df8c0e6b01928ad145e75
Moe Pad <moepad380@googlemail.com>
2017-08-14 18:56:48 +0200

    fixed name issue

--- src/main/kotlin/com/esentri/ex2/Activator.kt
+++ src/main/kotlin/com/esentri/ex2/Activator.kt
@@ -1,18 +1,16 @@
 package com.esentri.ex2
 
+import com.esentri.ex2.service.DictionaryService
 import org.osgi.framework.BundleActivator
 import org.osgi.framework.BundleContext
+import java.util.Hashtable
 
 
 class Activator: BundleActivator {
   override fun start(context: BundleContext) {
-//    try{
-//      val props = Hashtable<String, String>()
-//      props.put("Language", "English")
-//      context.registerService(DictionaryService::class.simpleName, DictionaryServiceImpl(), props)
-//    } catch(e: Exception) {
-//      println(e.stackTrace)
-//    }
+     val props = Hashtable<String, String>()
+     props.put("Language", "English")
+     context.registerService(DictionaryService::class.java.name, DictionaryServiceImpl(), props)
   }
 
   override fun stop(context: BundleContext) {
