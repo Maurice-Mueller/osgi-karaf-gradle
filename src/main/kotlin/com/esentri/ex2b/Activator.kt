0be1187cc07981427153c2099abdbd0ff4236651
Moe Pad <moepad380@googlemail.com>
2017-08-14 18:58:29 +0200

    changed props for service

--- src/main/kotlin/com/esentri/ex2b/Activator.kt
+++ src/main/kotlin/com/esentri/ex2b/Activator.kt
@@ -9,7 +9,7 @@ import java.util.*
 class Activator: BundleActivator {
   override fun start(context: BundleContext) {
       val props = Hashtable<String, String>()
-      props.put("Language", "English")
+      props.put("Language", "German")
       context.registerService(DictionaryService::class.java.name, DictionaryServiceImpl(), props)
   }
 
